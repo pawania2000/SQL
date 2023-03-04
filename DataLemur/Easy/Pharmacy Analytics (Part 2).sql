3d8b6e796f8befd1d712d6554ebf48cfd92e0b2d
Pawani A <123441470+pawania2000@users.noreply.github.com>
2023-03-04 12:53:23 +0530

    Update Pharmacy Analytics (Part 2).sql

--- DataLemur/Easy/Pharmacy Analytics (Part 2).sql
+++ DataLemur/Easy/Pharmacy Analytics (Part 2).sql
@@ -1,9 +1,8 @@
-/*CVS Health is trying to better understand its pharmacy sales, and how well different products are selling. Each drug can only be produced 
-by one manufacturer.
+/*CVS Health is trying to better understand its pharmacy sales, and how well different products are selling. Each drug can only be produced by one manufacturer.
 Write a query to find out which manufacturer is associated with the drugs that were not profitable and how much money CVS lost on these drugs. 
-Output the manufacturer, number of drugs and total losses. Total losses should be in absolute value. Display the results with the highest losses on top.
+Output the manufacturer, number of drugs and total losses. Total losses should be in absolute value. Display the results with the highest losses on top.*/
 
-I have answered  SQL question that was asked in a job interview at CVS Health.*/
+--I have answered  SQL question that was asked in a job interview at CVS Health.
 
 SELECT manufacturer , 
   count(drug) as drug_count , 
