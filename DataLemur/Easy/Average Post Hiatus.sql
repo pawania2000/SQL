f9e8585816fd7af22876d6eacc2907524693877a
Pawani A <123441470+pawania2000@users.noreply.github.com>
2023-03-04 12:47:29 +0530

    Update Average Post Hiatus.sql

--- DataLemur/Easy/Average Post Hiatus.sql
+++ DataLemur/Easy/Average Post Hiatus.sql
@@ -1,8 +1,8 @@
 /*Given a table of Facebook posts, for each user who posted at least twice in 2021, write a query to find the number of days between each user’s
  first post of the year and last post of the year in the year 2021. Output the user and number of the days between each user's first and last 
- post.
+ post.*/
 
-I have answered  SQL question that was asked in a job interview at Facebook.*/
+--I have answered  SQL question that was asked in a job interview at Facebook.
 
 SELECT 
   user_id, 
