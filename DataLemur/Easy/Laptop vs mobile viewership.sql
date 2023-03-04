15556b78a2a05db0a62d469351e2767326f0291f
Pawani A <123441470+pawania2000@users.noreply.github.com>
2023-03-04 12:50:47 +0530

    Update Laptop vs mobile viewership.sql

--- DataLemur/Easy/Laptop vs mobile viewership.sql
+++ DataLemur/Easy/Laptop vs mobile viewership.sql
@@ -1,9 +1,8 @@
-/*Assume that you are given the table below containing information on viewership by device type (where the three types are laptop, tablet, 
-and phone). Define “mobile” as the sum of tablet and phone viewership numbers. Write a query to compare the viewership on laptops versus 
-mobile devices.
-Output the total viewership for laptop and mobile devices in the format of "laptop_views" and "mobile_views".
+/*Assume that you are given the table below containing information on viewership by device type (where the three types are laptop, tablet, and phone). Define 
+“mobile” as the sum of tablet and phone viewership numbers. Write a query to compare the viewership on laptops versus mobile devices.
+Output the total viewership for laptop and mobile devices in the format of "laptop_views" and "mobile_views".*/
 
-I have answered  SQL question that was asked in a job interview at New York Times.*/
+--I have answered  SQL question that was asked in a job interview at New York Times.
 
 --Approach 1
 
