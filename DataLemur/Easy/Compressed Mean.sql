e4617d09f0a0380d4e777e64ffc369d88f3d24df
Pawani A <123441470+pawania2000@users.noreply.github.com>
2023-03-04 12:48:15 +0530

    Update Compressed Mean.sql

--- DataLemur/Easy/Compressed Mean.sql
+++ DataLemur/Easy/Compressed Mean.sql
@@ -1,9 +1,8 @@
 /*You are trying to find the mean number of items bought per order on Alibaba, rounded to 1 decimal place.
+However, instead of doing analytics on all Alibaba orders, you have access to a summary table, which describes how many items were in an order(item_count), and the 
+number of orders that had that many items (order_occurrences).*/
 
-However, instead of doing analytics on all Alibaba orders, you have access to a summary table, which describes how many items were in an order
-(item_count), and the number of orders that had that many items (order_occurrences).
-
-I have answered  SQL question that was asked in a job interview at Alibaba.*/
+--I have answered  SQL question that was asked in a job interview at Alibaba.
 
 --Approach 1
 
