64bfa2f045f532743372fbf7f694c7d18197a8e1
Pawani A <123441470+pawania2000@users.noreply.github.com>
2023-03-04 12:16:30 +0530

    Add files via upload

--- DataLemur/Easy/Data Science Skills.sql
+++ DataLemur/Easy/Data Science Skills.sql
@@ -1,1 +1,20 @@
+/*Given a table of candidates and their skills, you're tasked with finding the candidates best suited for an open Data Science job. 
+You want to find candidates who are proficient in Python, Tableau, and PostgreSQL.
 
+I have answered  SQL question that was asked in a job interview at LinkedIn.*/
+
+--Approach 1
+
+select distinct candidate_id 
+from candidates
+where skill IN('Python','Tableau','PostgreSQL')
+GROUP BY candidate_id
+having count(skill)=3;
+
+--Approach 2
+
+SELECT candidate_id
+FROM candidates
+WHERE skill IN ('Python', 'Tableau', 'PostgreSQL')
+GROUP BY candidate_id
+HAVING COUNT(DISTINCT skill) = 3;
