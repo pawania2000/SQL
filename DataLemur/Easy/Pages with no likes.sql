e8209fa2fe623edb54565e2016c2aae50f4e3d89
Pawani A <123441470+pawania2000@users.noreply.github.com>
2023-03-04 12:19:50 +0530

    Add files via upload

---
+++ DataLemur/Easy/Pages with no likes.sql
@@ -0,0 +1,10 @@
+/*Assume you are given the tables below about Facebook pages and page likes. Write a query to return the page IDs of all the Facebook pages 
+that don't have any likes. The output should be in ascending order.
+
+I have answered  SQL question that was asked in a job interview at Facebook.*/
+
+SELECT p.page_id
+FROM pages p
+left join page_likes l
+on p.page_id = l.page_id
+where l.page_id is null ;
