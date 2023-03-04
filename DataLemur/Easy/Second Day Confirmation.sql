3599f5aa7654c4d8142cf9134775e976bb850c2c
Pawani A <123441470+pawania2000@users.noreply.github.com>
2023-03-04 12:30:54 +0530

    Add files via upload

---
+++ DataLemur/Easy/Second Day Confirmation.sql
@@ -0,0 +1,14 @@
+/*New TikTok users sign up with their emails and each user receives a text confirmation to activate their account. Assume you are given the
+below tables about emails and texts.
+Write a query to display the ids of the users who did not confirm on the first day of sign-up, but confirmed on the second day.
+
+Assumption:
+
+•action_date is the date when the user activated their account and confirmed their sign-up through the text.
+I have answered  SQL question that was asked in a job interview at Tik Tok.*/
+
+SELECT user_id 
+FROM emails e
+join texts t
+on e.email_id = t.email_id
+where t.action_date = e.signup_date + INTERVAL '1 day'
