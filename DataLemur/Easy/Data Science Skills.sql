a8958d97f88fc40c8d1ff72576f60ce912d58d25
Pawani A <123441470+pawania2000@users.noreply.github.com>
2023-03-04 12:48:48 +0530

    Update Data Science Skills.sql

--- DataLemur/Easy/Data Science Skills.sql
+++ DataLemur/Easy/Data Science Skills.sql
@@ -1,7 +1,7 @@
 /*Given a table of candidates and their skills, you're tasked with finding the candidates best suited for an open Data Science job. 
-You want to find candidates who are proficient in Python, Tableau, and PostgreSQL.
+You want to find candidates who are proficient in Python, Tableau, and PostgreSQL.*/
 
-I have answered  SQL question that was asked in a job interview at LinkedIn.*/
+--I have answered  SQL question that was asked in a job interview at LinkedIn.
 
 --Approach 1
 
