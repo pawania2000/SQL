aec7ba1fce1647f7a86941ca8e1e75f709d12054
Pawani A <123441470+pawania2000@users.noreply.github.com>
2023-03-04 12:54:04 +0530

    Update Pharmacy Analytics (Part 3).sql

--- DataLemur/Easy/Pharmacy Analytics (Part 3).sql
+++ DataLemur/Easy/Pharmacy Analytics (Part 3).sql
@@ -1,11 +1,9 @@
-/*CVS Health is trying to better understand its pharmacy sales, and how well different products are selling. Each drug can only be produced 
-by one manufacturer.
+/*CVS Health is trying to better understand its pharmacy sales, and how well different products are selling. Each drug can only be produced by one manufacturer.
 Write a query to find the total sales of drugs for each manufacturer. Round your answer to the closest million, and report your results 
 in descending order of total sales.
-Because this data is being directly fed into a dashboard which is being seen by business stakeholders, format your result like this: "$36 million".
+Because this data is being directly fed into a dashboard which is being seen by business stakeholders, format your result like this: "$36 million".*/
 
-
-I have answered  SQL question that was asked in a job interview at CVS Health.*/
+--I have answered  SQL question that was asked in a job interview at CVS Health.
 
 --Approach 1
 
